5e1aca0c8bd83a512e5be761d19c08a9c1fc8337
Qingchu1995 <jinqingchu@hotmail.com>
2020-02-10 23:32:17 -0500

    perform dll on python demo

---
+++ demo_dll.py
@@ -0,0 +1,7 @@
+# An example of using dll
+from ctypes import *
+mydll = cdll.LoadLibrary("poker_64.dll")
+a1 = c_bool(0) #false
+a2 = c_bool(1) #true
+mydll.play_2p(a1,a2) # play_2p(bool unequalF, bool printF)
+mydll.play(a2) # play(bool printF)
