247de26e20421824f2215c70cbb08c7aebf60ad5
Qingchu1995 <jinqingchu@hotmail.com>
2020-02-11 15:56:58 -0500

    Update demo_dll.py

--- demo_dll.py
+++ demo_dll.py
@@ -1,6 +1,6 @@
 # An example of using dll
 from ctypes import *
-mydll = cdll.LoadLibrary("poker_64.dll")
+mydll = cdll.LoadLibrary("poker64.dll")
 a1 = c_bool(0) #false
 a2 = c_bool(1) #true
 mydll.play_2p(a1,a2) # play_2p(bool unequalF, bool printF)
